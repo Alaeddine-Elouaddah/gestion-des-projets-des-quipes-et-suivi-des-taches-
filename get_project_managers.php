fa1badac6f4a3929ccf7d8d7dca0b8f172036578
Alaeddine-Elouaddah <166127907+Alaeddine-Elouaddah@users.noreply.github.com>
2024-09-21 18:54:21 +0100

    Create get_project_managers.php

---
+++ get_project_managers.php
@@ -0,0 +1,30 @@
+<?php
+// get_project_details.php
+
+// Connexion à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gestionprojet";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$id = $_GET['id'];
+
+$sql = "SELECT * FROM projects WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+$project = $result->fetch_assoc();
+
+echo json_encode($project);
+
+$stmt->close();
+$conn->close();
+?>
